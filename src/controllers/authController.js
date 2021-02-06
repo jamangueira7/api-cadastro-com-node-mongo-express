7d11c55c9b00edf862a8cf67c076dab2de60ab74
João Mangueira <jamjs7@gmail.com>
2021-02-06 10:45:08 -0300

    Projeto está cadastrando usuario

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     try {
-        const user = await User.create(res.body);
+        const user = await User.create(req.body);
 
         return res.send({ user });
     } catch (err) {
-        return res.status(400).send({ error: 'Registration failed' });
+        return res.status(400).send({ error: 'Registration failed', msg: err.message });
     }
 });
 
