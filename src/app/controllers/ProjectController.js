10f970bfd1eb6a40770606ac675ab73c70ea9b28
João Mangueira <jamjs7@gmail.com>
2021-02-09 10:16:14 -0300

    estrutura do controle de projetos

--- src/app/controllers/ProjectController.js
+++ src/app/controllers/ProjectController.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
+    res.send({ ok: true });
+});
+
+router.get('/:projectId', async (req, res) => {
+    res.send({ ok: true });
+});
+
+router.post('/', async (req, res) => {
+    res.send({ ok: true });
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({ ok: true });
+});
+
+router.delete('/:projectId', async (req, res) => {
     res.send({ ok: true });
 });
 
