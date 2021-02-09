2a4d7139633106f59607c1e64ba127e0500e049b
João Mangueira <jamjs7@gmail.com>
2021-02-09 11:11:41 -0300

    add a rota de criação de projetos a criação de tasks tbm

--- src/app/controllers/ProjectController.js
+++ src/app/controllers/ProjectController.js
@@ -30,7 +30,18 @@ router.get('/:projectId', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const project = await Project.create({ ...req.body, user: req.userId });
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.create({ title, description, user: req.userId });
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id });
+
+            await projectTask.save();
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
 
         return res.send({ project });
     } catch (err) {
